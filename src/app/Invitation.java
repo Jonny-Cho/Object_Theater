a8d57ad76e92730c439b15c3f07bfcc0fa4e132d
Jonny-Cho <andante2183@gmail.com>
2019-07-04 17:36:51 +0900

    Theater ver.1

---
+++ src/app/Invitation.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Invitation{
+	final static public Invitation EMPTY = new Invitation(null);
+	final private Theater theater;
+	public Invitation(Theater theater) {
+		this.theater = theater;
+	}
+}
